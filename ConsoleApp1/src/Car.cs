2fc08f295cfdd28cfab613f11fbf05bedad2fcf1
mark0098 <crutoy.stalin@gmail.com>
2025-03-29 14:08:57 +0300

    Класс Car добавлен

---
+++ ConsoleApp1/src/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.src
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public int EnginePower { get; set; }
+
+        public Car(string model, decimal price, int enginePower)
+        {
+            Model = model;
+            Price = price;
+            EnginePower = enginePower;
+        }
+
+        public override string ToString()
+        {
+            return $"Model: {Model}, Price: {Price}, Engine Power: {EnginePower}";
+        }
+    }
+}
